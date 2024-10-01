48f858cb9dcc52784cdc368c07e05c1051a43a79
miriamwanner <78835598+miriamwanner@users.noreply.github.com>
2024-10-01 13:54:17 -0400

    Update decomp_script_example.sh

--- decomp_script_example.sh
+++ decomp_script_example.sh
@@ -6,7 +6,7 @@ export PATH_TO_ENV=/home/mwanner5/miniconda3/envs/factscore/bin
 export PATH_TO_HOME=/brtx/602-nvme1/mwanner5
 export PYTHONPATH=${PATH_TO_HOME}/decontext-decomp
 
-${PATH_TO_ENV}/python ${PYTHONPATH}/decomp/decompose_second.py \
+${PATH_TO_ENV}/python ${PYTHONPATH}/decomp/decompose_prompt.py \
                 --input_path ${PYTHONPATH}/data/decontext-then-decomp/${FILENAME}_decontext_then_decomp.jsonl \
                 --openai_key ${PYTHONPATH}/data/gpt_key.txt \
                 --data_dir ${PYTHONPATH}/data \
