e727d3c5640649d40b00dea1f83c3bb0a9c49f35
miriamwanner <78835598+miriamwanner@users.noreply.github.com>
2025-10-29 09:39:27 -0400

    Update environment variables in decomp_script_example.sh

--- decomp_script_example.sh
+++ decomp_script_example.sh
@@ -2,9 +2,9 @@
 
 export FILENAME=Alpaca-65B
 
-export PATH_TO_ENV=/home/mwanner5/miniconda3/envs/factscore/bin
-export PATH_TO_HOME=/brtx/602-nvme1/mwanner5
-export PYTHONPATH=${PATH_TO_HOME}/decontext-decomp
+export PATH_TO_ENV=envs/factscore/bin
+export PATH_TO_HOME=~/
+export PYTHONPATH=factscore_utils
 
 ${PATH_TO_ENV}/python ${PYTHONPATH}/decomp/decompose_prompt.py \
                 --input_path ${PYTHONPATH}/data/decontext-then-decomp/${FILENAME}_decontext_then_decomp.jsonl \
